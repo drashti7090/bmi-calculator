0282c51fb9429486c315fcbeef9ba78081aec305
drashti7090 <136163315+drashti7090@users.noreply.github.com>
2024-01-05 14:12:42 +0530

    final calculator

---
+++ lib/answer_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  final double height;
+  final double weight;
+  final double age;
+  final double result;
+  final bool isMale;
+  final String message;
+
+  const ResultScreen({super.key,
+    required this.height,
+    required this.weight,
+    required this.age,
+    required this.result,
+    required this.isMale,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0b0f1f),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor:  Colors.indigo.shade400,
+        leading: IconButton(icon: const Icon(Icons.arrow_back_ios_rounded), color: Colors.white,onPressed: () {
+          Navigator.pop(context);
+        },),
+        title: const Text(
+          'BMI Result',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color(0xff1b1a2e),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Gender : ${isMale ? 'Male' : 'Female'}',
+                      style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'AGE : ${age.toInt()}',
+                      style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Height : ${height.toInt()}cm',
+                      style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Weight : ${weight.toInt()}kg',
+                      style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'BMI Result : ${result.toInt()}kg',
+                      style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color:  Colors.indigo.shade400,
+                      ),
+                      height: 70,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      child: Text(
+                        message,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
